87b0c1add9df1de29bd6ef6a0a7ef4c10d2c9d2e
David Hemingway <david@hemingweb.com>
2017-12-05 09:14:06 +0000

    analytics

---
+++ analyticstracking.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-8238770-1']);
+  _gaq.push(['_setDomainName', 'hemingweb.com']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://www' : 'http://www') + '.hemingweb.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
